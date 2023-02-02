a35197af7ea668447c31dbc1548a7a51cfa95db9
ach <ach@achdeiMac.local>
2023-02-02 23:22:43 +0800

    Count the Digit

---
+++ Fundamental/CountDig.cpp
@@ -0,0 +1,56 @@
+#include <gtest/gtest.h>
+
+/*Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+Square all numbers k (0 <= k <= n) between 0 and n.
+
+Count the numbers of digits d used in the writing of all the k**2.
+
+Call nb_dig (or nbDig or ...) the function taking n and d as parameters and returning this count.
+
+Examples:
+n = 10, d = 1
+the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+
+nb_dig(25, 1) returns 11 since
+        the k*k that contain the digit 1 are:
+1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+So there are 11 digits 1 for the squares of numbers between 0 and 25. */
+
+class CountDig {
+public:
+    static int nbDig(int n, int d);
+};
+
+// 关键是判断平方和里面有没有给定数字
+int count(int n, int d) {
+    int res = 0;
+    do {
+        int tmp = n % 10;
+        if (tmp == d) {
+            res++;
+        }
+        n /= 10;
+    } while (n > 0);
+    return res;
+}
+
+int CountDig::nbDig(int n, int d) {
+    int res = 0;
+    for (int i = 0; i <= n; i++) {
+        res += count(i * i, d);
+    }
+    return res;
+}
+
+TEST(TEST, nbDig) {
+    EXPECT_EQ(CountDig::nbDig(5750, 0), 4700);
+    EXPECT_EQ(CountDig::nbDig(11011, 2), 9481);
+    EXPECT_EQ(CountDig::nbDig(12224, 8), 7733);
+}
+
+int main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
